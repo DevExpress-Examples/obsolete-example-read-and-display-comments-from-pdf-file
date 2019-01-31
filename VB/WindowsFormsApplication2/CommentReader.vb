e758b735fd4b6a8d397a3507d15e8ceaa0fea1fa
DevExpressExampleBot <fakeEmail>
2019-01-31 22:04:10 +0300

    Source auto update [skip ci]

---
+++ VB/WindowsFormsApplication2/CommentReader.vb
@@ -0,0 +1,59 @@
+﻿Imports DevExpress.Pdf
+Imports System.Collections.Generic
+
+Namespace WindowsFormsApplication2
+    Public Class CommentReader
+        Public Function ReadComments(ByVal fileName As String) As List(Of CommentData)
+            Dim comments As New List(Of CommentData)()
+            Using pServer As New PdfDocumentProcessor()
+                pServer.LoadDocument(fileName)
+                For i As Integer = 0 To pServer.Document.Pages.Count - 1
+                    Dim page As PdfPage = pServer.Document.Pages(i)
+                    Dim pageNumber As Integer = i + 1
+                    For Each annotation As PdfAnnotation In page.Annotations
+                        If TypeOf annotation Is PdfTextAnnotation Then
+                            Dim comment As PdfTextAnnotation = CType(annotation, PdfTextAnnotation)
+                            If comment.InReplyTo Is Nothing Then
+                                If comments.FindIndex(Function(item) item.Name = comment.Name) = -1 Then
+                                    comments.Add(CreateCommentData(comment, pageNumber))
+                                End If
+                            Else
+                                Dim replyData As ReplyData = CreateReplyData(comment, pageNumber)
+                                Dim parentAnnotation As PdfTextAnnotation = CType(comment.InReplyTo, PdfTextAnnotation)
+                                Dim name As String = parentAnnotation.Name
+                                Dim parentComment As CommentData = comments.Find(Function(item) item.Name = name)
+                                If parentComment Is Nothing Then
+                                    parentComment = CreateCommentData(comment, pServer.Document.Pages.IndexOf(parentAnnotation.Page) + 1)
+                                    comments.Add(parentComment)
+                                End If
+                                parentComment.Replies.Add(replyData)
+                            End If
+                        End If
+                    Next annotation
+                Next i
+            End Using
+            Return comments
+        End Function
+        Private Function CreateReplyData(ByVal comment As PdfTextAnnotation, ByVal pageNumber As Integer) As ReplyData
+            Dim reply As New ReplyData()
+            InitData(reply, comment, pageNumber)
+            Return reply
+        End Function
+        Private Function CreateCommentData(ByVal comment As PdfTextAnnotation, ByVal pageNumber As Integer) As CommentData
+            Dim data As New CommentData()
+            InitData(data, comment, pageNumber)
+            Dim pageCropBoxBottomLeft As PdfPoint = comment.Page.CropBox.BottomLeft
+            Dim rectangle As New PdfRectangle(comment.Rect.Left - pageCropBoxBottomLeft.X, comment.Rect.Bottom - pageCropBoxBottomLeft.Y, comment.Rect.Right - pageCropBoxBottomLeft.X, comment.Rect.Top - pageCropBoxBottomLeft.Y)
+            data.BoundingBox = rectangle
+            Return data
+        End Function
+        Private Sub InitData(ByVal data As ReplyData, ByVal comment As PdfTextAnnotation, ByVal pageNumber As Integer)
+            data.Subject = comment.Title
+            data.Text = comment.Contents
+            data.Name = comment.Name
+            data.CreationDate = comment.CreationDate
+            data.PageNumber = pageNumber
+        End Sub
+
+    End Class
+End Namespace
---
+++ VB/WindowsFormsApplication2/CommentsPanel/CommentsPanelHelper.vb
@@ -0,0 +1,69 @@
+﻿Imports DevExpress.Pdf
+Imports DevExpress.XtraBars.Navigation
+Imports System
+Imports System.Collections.Generic
+Imports System.Drawing
+Imports System.Linq
+Imports System.Text
+Imports System.Threading.Tasks
+
+Namespace WindowsFormsApplication2
+    Public Class CommentsPanelHelper
+        Private Const imagePlaceholder As String = "<image =#Comment_16x16>"
+        Private acControl As AccordionControl
+        Public Sub New(ByVal acControl As AccordionControl)
+            Me.acControl = acControl
+            AddHandler acControl.CustomDrawElement, AddressOf AcControl_CustomDrawElement
+        End Sub
+
+        Private Sub AcControl_CustomDrawElement(ByVal sender As Object, ByVal e As CustomDrawElementEventArgs)
+            Dim selectedCommentName As String = If(acControl.Tag IsNot Nothing, acControl.Tag.ToString(), "")
+            If e.Element.Style = DevExpress.XtraBars.Navigation.ElementStyle.Group AndAlso e.Element.Name = selectedCommentName Then
+                Dim viewInfo As AccordionGroupViewInfo = TryCast(e.ObjectInfo, AccordionGroupViewInfo)
+                e.DrawHeaderBackground()
+                Using brush As Brush = New SolidBrush(Color.FromArgb(30, Color.Gray))
+                    e.Cache.FillRectangle(brush, viewInfo.HeaderBounds)
+                End Using
+                e.DrawText()
+                e.DrawImage()
+                e.DrawExpandCollapseButton()
+                e.Handled = True
+            End If
+        End Sub
+
+        Public Sub ClearPanel()
+            acControl.BeginUpdate()
+            acControl.Elements.Clear()
+            acControl.EndUpdate()
+        End Sub
+        Public Sub InitPanel(ByVal comments As List(Of CommentData))
+            acControl.BeginUpdate()
+
+            Dim fakeGroup As New AccordionControlElement()
+            fakeGroup.HeaderVisible = False
+            acControl.Elements.Add(fakeGroup)
+            For Each comment As CommentData In comments
+                Dim acgroup As AccordionControlElement = CreateReplyItem(comment, True, New PdfDocumentPosition(comment.PageNumber, comment.BoundingBox.TopLeft))
+                For Each reply As ReplyData In comment.Replies
+                    acgroup.Elements.Add(CreateReplyItem(reply, False, New PdfDocumentPosition(comment.PageNumber, comment.BoundingBox.TopLeft)))
+                Next reply
+                fakeGroup.Elements.Add(acgroup)
+            Next comment
+
+           acControl.EndUpdate()
+        End Sub
+
+        Private Function CreateReplyItem(ByVal reply As ReplyData, ByVal isGroupItem As Boolean, ByVal posiiton As PdfDocumentPosition) As AccordionControlElement
+            Dim acItem As New AccordionControlElement()
+            acItem.Style = If(isGroupItem, ElementStyle.Group, ElementStyle.Item)
+            acItem.Tag = posiiton
+            acItem.Name = reply.Name
+            Dim creationDate As DateTimeOffset = If(reply.CreationDate, Date.MinValue)
+            Dim htmlText As String = String.Format("{0} <b>{1}</b> {2}<br>{3}", imagePlaceholder, reply.Subject, creationDate.DateTime.ToShortDateString(), reply.Text)
+            acItem.Text = htmlText
+
+            acItem.Expanded = True
+            Return acItem
+        End Function
+    End Class
+End Namespace
